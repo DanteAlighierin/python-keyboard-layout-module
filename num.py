75079de617e6b3fedc0dc5ca982bbb07e7fe62be
Dante Alighierin <zhivchikovv2@gmail.com>
2020-10-02 22:40:56 +0300

    update libs compatible with python 3.x.x

--- num.py
+++ num.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
-import commands
-layout = commands.getoutput("xset -q|grep LED| awk '{ print $10 }' ")
+#!/usr/bin/env python3
+import subprocess
+layout = subprocess.check_output("xset -q|grep LED| awk '{ print $10 }' ", shell=True)
 if layout == '00000002':
 	print ("en")
 if layout=='00001006':
